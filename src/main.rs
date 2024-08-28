69edbdc748737bc65617c8b9f2ca99f389ec4aa0
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-28 13:00:07 +0100

    Continue building minimal example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
+use core::cell::Cell;
 
-use cortex_m::interrupt::{CriticalSection, Mutex};
+use cortex_m_rt::entry;
+use critical_section::Mutex;
 use nrf52832_hal::{
     self as hal,
     pac::{Peripherals, UARTE0},
@@ -13,22 +14,34 @@ use rtt_target::rprintln;
 use hal::pac::interrupt;
 
 #[repr(C)]
+#[derive(Copy, Clone)]
 struct Packet {
     payload: [u8; 5],
 }
 
 static mut RXD: [Packet; 10] = [Packet { payload: [0; 5] }; 10];
+static WRITE: Mutex<Cell<isize>> = Mutex::new(Cell::new(0));
+static READ: Mutex<Cell<isize>> = Mutex::new(Cell::new(0));
 
-static P: Mutex<RefCell<Option<Peripherals>>> = Mutex::new(RefCell::new(None));
+static P: Mutex<Cell<Option<Peripherals>>> = Mutex::new(Cell::new(None));
 
 #[interrupt]
 fn UARTE0_UART0() {
-    let cs = unsafe { CriticalSection::new() };
-    if let Some(peripherals) = P.borrow(&cs).take() {
-        if peripherals.UARTE0.events_rxstarted.read().bits() == 1 {
-            update_rxd_ptr(&peripherals.UARTE0);
+    critical_section::with(|cs| {
+        if let Some(peripherals) = P.borrow(cs).take() {
+            if peripherals.UARTE0.events_rxstarted.read().bits() == 1 {
+                rprintln!("RXSTARTED");
+                update_rxd_ptr(&peripherals.UARTE0);
+                peripherals.UARTE0.events_rxstarted.reset();
+            } else if peripherals.UARTE0.events_error.read().bits() == 1 {
+                rprintln!("ERROR");
+                peripherals
+                    .UARTE0
+                    .tasks_flushrx
+                    .write(|w| unsafe { w.bits(1) });
+            }
         }
-    }
+    })
 }
 
 #[entry]
@@ -39,11 +52,11 @@ fn main() -> ! {
     let peripherals = hal::pac::Peripherals::take().unwrap();
 
     rprintln!("Starting HF clock...");
-    let clock = peripherals.CLOCK;
+    let clock = &peripherals.CLOCK;
     clock.tasks_hfclkstart.write(|w| unsafe { w.bits(1) });
 
     rprintln!("Initialising UART...");
-    let uart = peripherals.UARTE0;
+    let uart = &peripherals.UARTE0;
     uart.psel.rxd.write(|w| {
         w.pin()
             .variant(3)
@@ -52,27 +65,25 @@ fn main() -> ! {
     });
     uart.baudrate.write(|w| w.baudrate().baud9600());
     uart.intenset
-        .write(|w| w.rxstarted().set_bit().error().set_bit());
+        .write(|w| w.rxstarted().set_bit().error().set_bit().endrx().set_bit());
     unsafe { hal::pac::NVIC::unmask(hal::pac::Interrupt::UARTE0_UART0) };
 
     uart.shorts.write(|w| w.endrx_startrx().set_bit());
     update_rxd_ptr(&uart);
     uart.rxd.maxcnt.write(|w| w.maxcnt().variant(5));
 
-    {
-        let cs = unsafe { CriticalSection::new() };
-        P.borrow(&cs).replace(Some(peripherals));
-    }
+    critical_section::with(|cs| P.borrow(cs).replace(Some(peripherals)));
 
     loop {}
 }
 
 fn update_rxd_ptr(uart: &UARTE0) {
-    static mut WRITE: usize = 0;
-
-    uart.rxd.ptr.write(|w| {
-        w.ptr()
-            .variant(core::ptr::addr_of!(unsafe { RXD[WRITE] }) as u32)
+    critical_section::with(|cs| {
+        let write_offset = WRITE.borrow(cs).get();
+        uart.rxd.ptr.write(|w| {
+            w.ptr()
+                .variant(unsafe { RXD.as_ptr().offset(write_offset) } as u32)
+        });
+        WRITE.borrow(cs).set((write_offset + 1) % 10);
     });
-    unsafe { WRITE = (WRITE + 1) % 10 };
 }
