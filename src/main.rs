a1ac2c312535e9d118dd59eb601d3e97240d6642
Peter Kubiak <peter.s.kubiak@bath.edu>
2024-08-29 00:44:48 +0100

    First working RX example

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,11 @@ use nrf52832_hal::{
     self as hal,
     pac::{Peripherals, UARTE0},
 };
-use rtt_target::rprintln;
+use rtt_target::{rprint, rprintln};
 
 use hal::pac::interrupt;
+
+extern crate panic_halt;
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -19,9 +21,19 @@ struct Packet {
     payload: [u8; 5],
 }
 
+impl Packet {
+    fn as_str(&self) -> Result<&str, &str> {
+        if let Ok(string) = core::str::from_utf8(&self.payload) {
+            Ok(string)
+        } else {
+            Err("Packet contains non-ascii characters!")
+        }
+    }
+}
+
 static mut RXD: [Packet; 10] = [Packet { payload: [0; 5] }; 10];
 static WRITE: Mutex<Cell<isize>> = Mutex::new(Cell::new(0));
-static READ: Mutex<Cell<isize>> = Mutex::new(Cell::new(0));
+static READ: Mutex<Cell<usize>> = Mutex::new(Cell::new(0));
 
 static P: Mutex<Cell<Option<Peripherals>>> = Mutex::new(Cell::new(None));
 
@@ -30,16 +42,28 @@ fn UARTE0_UART0() {
     critical_section::with(|cs| {
         if let Some(peripherals) = P.borrow(cs).take() {
             if peripherals.UARTE0.events_rxstarted.read().bits() == 1 {
-                rprintln!("RXSTARTED");
                 update_rxd_ptr(&peripherals.UARTE0);
                 peripherals.UARTE0.events_rxstarted.reset();
             } else if peripherals.UARTE0.events_error.read().bits() == 1 {
-                rprintln!("ERROR");
                 peripherals
                     .UARTE0
                     .tasks_flushrx
                     .write(|w| unsafe { w.bits(1) });
+                peripherals.UARTE0.events_error.reset();
+            } else if peripherals.UARTE0.events_endrx.read().bits() == 1 {
+                critical_section::with(|cs| {
+                    let index = READ.borrow(cs).get();
+                    let data = unsafe { RXD[index] };
+                    match data.as_str() {
+                        Ok(chars) => rprint!("{}", chars),
+                        Err(error_message) => rprintln!("\nError: {}", error_message),
+                    }
+
+                    READ.borrow(cs).set((index + 1) % 10);
+                    peripherals.UARTE0.events_endrx.reset();
+                })
             }
+            P.borrow(cs).set(Some(peripherals));
         }
     })
 }
@@ -57,11 +81,15 @@ fn main() -> ! {
 
     rprintln!("Initialising UART...");
     let uart = &peripherals.UARTE0;
+    uart.enable.write(|w| {
+        w.enable()
+            .variant(hal::pac::uarte0::enable::ENABLE_A::ENABLED)
+    });
     uart.psel.rxd.write(|w| {
         w.pin()
             .variant(3)
             .connect()
-            .variant(nrf52832_hal::pac::uarte0::psel::rxd::CONNECT_A::CONNECTED)
+            .variant(hal::pac::uarte0::psel::rxd::CONNECT_A::CONNECTED)
     });
     uart.baudrate.write(|w| w.baudrate().baud9600());
     uart.intenset
@@ -69,12 +97,22 @@ fn main() -> ! {
     unsafe { hal::pac::NVIC::unmask(hal::pac::Interrupt::UARTE0_UART0) };
 
     uart.shorts.write(|w| w.endrx_startrx().set_bit());
-    update_rxd_ptr(&uart);
+    update_rxd_ptr(uart);
     uart.rxd.maxcnt.write(|w| w.maxcnt().variant(5));
 
-    critical_section::with(|cs| P.borrow(cs).replace(Some(peripherals)));
+    rprintln!("Starting UART");
 
-    loop {}
+    critical_section::with(|cs| {
+        peripherals
+            .UARTE0
+            .tasks_startrx
+            .write(|w| unsafe { w.bits(1) });
+        P.borrow(cs).replace(Some(peripherals))
+    });
+
+    loop {
+        cortex_m::asm::wfi();
+    }
 }
 
 fn update_rxd_ptr(uart: &UARTE0) {
